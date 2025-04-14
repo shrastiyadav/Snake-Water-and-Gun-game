62a79ea1f24fe7df5afe6bcb118496b98fb8b475
shrasti yadav <ysrishti163@gmail.com>
2025-04-14 18:22:59 +0530

    Initial commit: Snake Water Gun game with README

---
+++ main-shortened.py
@@ -0,0 +1,48 @@
+'''
+1 for snake 
+-1 for water
+0 for gun
+'''
+import random
+
+computer = random.choice([1,-1,0])
+
+youstr = input("enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1:"Snake", -1: "water",0:"gun"}
+you= youDict[youstr] 
+
+#By now We have 2 numbers ,you and computer
+
+print(f"you choose {reverseDict[you]}\ncomputer choose {reverseDict[computer]}")
+
+if(computer==you):
+    print("its a Draw")
+
+else:
+    '''
+    if(computer == -1 and you ==1): -2
+        print("you win !")
+
+    elif(computer== -1 and you == 0): -1
+        print("you lose")
+
+    elif(computer== 1 and you == -1): 2
+        print("you lose")
+
+    elif(computer== 1 and you == 0): 1
+        print("you win !")
+
+    elif(computer== 0 and you == 1): -1
+        print("you lose")
+
+    elif(computer== 0 and you == -1): 1
+        print("you win !")
+
+    else:
+        print("something went wrong !")
+'''
+if((computer-you) == -1 or (computer-you) == 2):
+    print("You Lose!")
+else:
+    print("You Win!")
